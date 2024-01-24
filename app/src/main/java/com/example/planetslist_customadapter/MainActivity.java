6a12957c0d2cc6b5deb88f533ba0b9de3ac44dab
hemanathan18 <hemanathan1802@gmail.com>
2024-01-24 20:41:07 +0530

    updates: planet object added and clicklistener functionality added

--- app/src/main/java/com/example/planetslist_customadapter/MainActivity.java
+++ app/src/main/java/com/example/planetslist_customadapter/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.planetslist_customadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,10 +23,36 @@ public class MainActivity extends AppCompatActivity {
         ListView listView=findViewById(R.id.listView);
         planetArrayList=new ArrayList<>();
 
+
+        Planet mercury=new Planet("Mercury","0 Moon",R.drawable.mercury);
+        Planet venus=new Planet("Venus","0 Moon",R.drawable.venus);
         Planet earth=new Planet("Earth","1 Moon",R.drawable.earth);
+        Planet mars=new Planet("Mars","2 Moons",R.drawable.mars);
+        Planet jupiter=new Planet("Jupiter","951 Moons",R.drawable.jupiter);
+        Planet saturn=new Planet("Saturn","146 Moons",R.drawable.saturn);
+        Planet uranus=new Planet("Uranus","27 Moons",R.drawable.uranus);
+        Planet neptune=new Planet("Neptune","14 Moons",R.drawable.neptune);
+
+        planetArrayList.add(mercury);
+        planetArrayList.add(venus);
         planetArrayList.add(earth);
+        planetArrayList.add(mars);
+        planetArrayList.add(jupiter);
+        planetArrayList.add(saturn);
+        planetArrayList.add(uranus);
+        planetArrayList.add(neptune);
+
 
         adapter=new CustomAdapter(planetArrayList,getApplicationContext());
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Planet name: "+adapter.getItem(position).getPlanetName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
